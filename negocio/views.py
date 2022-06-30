9af405904c771e86b4847ddbe45f32fb9bcdebad
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2022-06-29 22:08:42 -0300

    falta salvar o agendamento

--- negocio/views.py
+++ negocio/views.py
@@ -2,14 +2,36 @@ from django.contrib import messages
 from django.contrib.messages import constants
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-from .models import Agendamento
+from .models import Agendamento ,Horario , Dia ,Funcionario
 from django.contrib import auth
 
 
 def agendamentos(request):
-    agenda = Agendamento.objects.all().filter(status='A')
+    usuario = request.user
+    agenda = Agendamento.objects.all().filter(status='A').filter(usuario=usuario)
     return render(request, 'agendamento.html',{'agenda':agenda})
 
 def agendar(request):
-    
-    return render(request, 'agendar.html')
+    if request.session.get('GET'):
+        return render(request, 'agendar.html')
+    else:
+        horas = Horario.objects.all()
+        dias = Dia.objects.all()
+        funcionarios = Funcionario.objects.all()
+        
+        dia = request.POST.get('dias')
+        hora = request.POST.get('hora')
+        funcionario = request.POST.get('funcionario')
+        usuario = request.user
+        
+        Agenda = Agendamento(Funcionario = funcionario,
+                                    usuario = usuario,
+                                    dia = dia,
+                                    horario = hora,
+                                    )
+
+        Agenda.save()
+
+        return render(request, 'agendar.html',{'dias':dias,
+                                                'horas' :horas,
+                                                'funcionarios':funcionarios})
